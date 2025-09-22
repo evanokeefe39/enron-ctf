5bb42e17c63e9e50e8a1c8046858f834443ef4d9
evanokeefe39 <evan.okeefe39@gmail.com>
2025-09-22 16:36:09 +0200

    clean data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import kagglehub
 
 import shutil
 
-# Load the latest version
+# TODO: upload custom dataset, figure out why binary when csv gets downloaded
 path = kagglehub.dataset_download(
   
   "wcukierski/enron-email-dataset",
@@ -10,6 +10,6 @@ path = kagglehub.dataset_download(
  
 )
 
-shutil.move(path, "./sample_data/emails.csv")
+print(f"downloaded {path}")
 
-
+# shutil.move(path, "./sample_data/emails.csv")
